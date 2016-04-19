eac1e0586d183fc1a11966ab4a96ea25231d793e
paulo <pjs.figueira@hotmail.com>
2016-04-19 20:14:36 +0100

    updated

--- src/WhiteChapel.java
+++ src/WhiteChapel.java
@@ -34,7 +34,7 @@ public class WhiteChapel {
         boolean [][] intersection = new boolean[hideoutDistances.length][graph.length];
         List<Integer> list=new LinkedList<>();
         for(int i = 0;i<hideoutDistances.length;i++){
-            if((list=bfsExplore(found,intersection,hideoutDistances[i],i))==null){
+            if((bfsExplore(found,intersection,hideoutDistances[i],i).size()==0)){
                 return null;
             }
         }
@@ -45,39 +45,41 @@ public class WhiteChapel {
     private List<Integer> bfsExplore(boolean[] found, boolean[][] intersection, Clue root,int matrixLine) {
         Queue<Integer> waiting = new LinkedList<>();
         Queue<Integer> adjacentNodes = new LinkedList<>();
-        int roads = -1;
         List<Integer> list=new LinkedList<>();
         waiting.add(root.getCrimeLoc());
         found[root.getCrimeLoc()]=true;
-        boolean stop=false;
-        do{
 
 
-
-                for (int i=0; i<root.getRoadClue();i++){
-
-
-                    while(!waiting.isEmpty()){
+            for (int i = 0; i < root.getRoadClue(); i++) {
 
 
-                        for (int node :graph[waiting.remove()]) {
+                while (!waiting.isEmpty()) {
 
-                            if(!found[node]){
-                                found[node]=true;
-                                adjacentNodes.add(node);
+                    for (int node : graph[waiting.remove()]) {
+
+                        if (!found[node]) {
+                            if (i == root.getRoadClue() - 1) {
+                                if (matrixLine > 0) {
+                                    if (intersection[matrixLine - 1][node]) {
+                                        intersection[matrixLine][node] = intersection[matrixLine - 1][node];
+                                        list.add(node);
+                                    }
+
+                                } else intersection[matrixLine][node] = true;
 
                             }
+                            found[node] = true;
+                            adjacentNodes.add(node);
                         }
+
                     }
-                    waiting.addAll(adjacentNodes);
-                    adjacentNodes.clear();
-
                 }
-
-
-
-
-                roads++;
+                waiting.addAll(adjacentNodes);
+                adjacentNodes.clear();
+            }
+            return list;
+             /*
+             do{roads++;
                 int numberOfAdj = graph[node].size();
                 for(Integer vertex : graph[node]){
                     if(!found[vertex]){
@@ -105,5 +107,6 @@ public class WhiteChapel {
 
         }while(!waiting.isEmpty() && !stop);
         return list;
+    */
     }
 }
